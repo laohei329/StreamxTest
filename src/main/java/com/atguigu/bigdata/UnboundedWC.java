93c955b5b80fc6601522931848298175f7f6836c
MrSun <290834162@qq.com>
2022-10-26 15:02:44 +0800

    注释

--- src/main/java/com/atguigu/bigdata/UnboundedWC.java
+++ src/main/java/com/atguigu/bigdata/UnboundedWC.java
@@ -15,34 +15,34 @@ public class UnboundedWC {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
-        
+
         env
-            .socketTextStream("10.10.101.62", 9999)
-            .flatMap(new FlatMapFunction<String, String>() {
-                @Override
-                public void flatMap(String line,
-                                    Collector<String> out) throws Exception {
-                    for (String word : line.split(" ")) {
-                        out.collect(word);
-                    }
-                }
-            })
-            .map(new MapFunction<String, Tuple2<String, Long>>() {
-                @Override
-                public Tuple2<String, Long> map(String word) throws Exception {
-                    return Tuple2.of(word, 1l);
-                }
-            })
-            .keyBy(new KeySelector<Tuple2<String, Long>, String>() {
-                @Override
-                public String getKey(Tuple2<String, Long> t) throws Exception {
-                    return t.f0;  // t._1
-                }
-            })
-            .sum(1)
-            .print();
-        
+                .socketTextStream("10.10.101.62", 9999)
+//                .flatMap(new FlatMapFunction<String, String>() {
+//                    @Override
+//                    public void flatMap(String line,
+//                                        Collector<String> out) throws Exception {
+//                        for (String word : line.split(" ")) {
+//                            out.collect(word);
+//                        }
+//                    }
+//                })
+//                .map(new MapFunction<String, Tuple2<String, Long>>() {
+//                    @Override
+//                    public Tuple2<String, Long> map(String word) throws Exception {
+//                        return Tuple2.of(word, 1l);
+//                    }
+//                })
+//                .keyBy(new KeySelector<Tuple2<String, Long>, String>() {
+//                    @Override
+//                    public String getKey(Tuple2<String, Long> t) throws Exception {
+//                        return t.f0;  // t._1
+//                    }
+//                })
+//                .sum(1)
+                .print();
+
         env.execute();
-        
+
     }
 }
